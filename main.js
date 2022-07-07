adff54b47b23ca4b48145ecc4409a2c79175f340
ezequiel-piki <ezequielpiki23@gmail.com>
2022-07-06 22:21:41 -0300

    no me acuerdo que paso

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const template = document.getElementById('template');
 const fragment = new DocumentFragment();
 const botones  = document.querySelectorAll('.card .btn');
 
-const carritoObjeto = {};
+const carritoObjeto = [];
 
 const agregarAlCarrito = (event)=>{
   console.log(event.target.dataset.fruta);
@@ -14,22 +14,31 @@ const agregarAlCarrito = (event)=>{
     cantidad:1
   }
 
-  if(carritoObjeto.hasOwnProperty(producto.titulo)){
-  producto.cantidad = carritoObjeto[producto.titulo].cantidad + 1;
+  const indice = carritoObjeto.findIndex((item)=>
+  item.id === producto.id
+  )
+
+  console.log(indice)
+
+  if(indice === -1){
+    carritoObjeto.push(producto)
+  } else{
+    carritoObjeto[indice].cantidad ++;
   }
 
-  carritoObjeto[producto.titulo]=producto;
+  console.log(carritoObjeto)
 
-  pintarCarrito(producto);
+  pintarCarrito(carritoObjeto); 
   
   /* console.log(carritoObjeto) */
-}
+ 
+ }
 
-const pintarCarrito = (producto) =>{
+const pintarCarrito = (array) =>{
     
     carrito.textContent=''
 
-    Object.values(carritoObjeto).forEach(item =>{
+    array.forEach(item =>{
         const clone = template.content.firstElementChild.cloneNode(true);
         clone.querySelector('.lead').textContent=item.titulo
         clone.querySelector('.badge').textContent=item.cantidad
@@ -47,4 +56,5 @@ botones.forEach((boton)=>{
     boton.addEventListener('click',agregarAlCarrito)
 
 })
+  
 
